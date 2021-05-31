dd4cbad71d6f1ca4095c288fdfae5b1e88714bbe
Abhishek Shukla <as841840@student.nitw.ac.in>
2021-05-31 20:25:34 +0530

    added regression part

--- ProjectML/app.py
+++ ProjectML/app.py
@@ -15,7 +15,7 @@ app.config["UPLOAD_PATH"]=cwd
 
 @app.route("/index")
 def index_page():
-    	return render_template("index.html");
+		return render_template("index.html");
 
 @app.route("/classification",methods=["GET","POST"])  #Decorator
 def home_page():
@@ -41,7 +41,7 @@ def evaluate():
 
 	print(model_file)
 	print(dataset_file)
-	
+
 	with open(dataset_file) as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter = ',')
 		list_of_column_names = []
@@ -72,11 +72,76 @@ def evaluate():
 	return render_template("evaluate.html",msg="files have been Uploaded",a=acc,p=precision_score,r=recall,f1=f1,log_loss=log_loss)
 
 
+@app.route('/regression',methods=["GET","POST"])
+def regression():
+	if request.method=='POST':
+		f1=request.files['modelname']
+		f2=request.files['test_data']
+		f1.save(os.path.join(app.config['UPLOAD_PATH'],f1.filename))
+		f2.save(os.path.join(app.config['UPLOAD_PATH'],f2.filename))
+		return redirect(url_for("evaluate_regression"))
+	else:
+		return render_template("regression.html",msg="Upload your files")
 
+@app.route('/evaluate_regression')
 def evaluate_regression():
-	pass
+	model_file=''
+	dataset_file=''
+	for filename in os.listdir(cwd):
+		if filename.endswith(".sav"):
+			model_file=filename
+		if filename.endswith(".csv"):
+			dataset_file=filename
 
+	print(model_file)
+	print(dataset_file)
 
+	with open(dataset_file) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter = ',')
+		list_of_column_names = []
+		for row in csv_reader:
+			list_of_column_names.append(row)
+			break
+	print(list_of_column_names[0])
+	loaded_model = pickle.load(open(model_file, 'rb'))
+	col_names = list_of_column_names[0]
+	dataset=pd.read_csv(dataset_file,header=None,names=col_names,skiprows=1)
+
+	feature_cols= ['ZN', 'INDUS', 'RM', 'AGE', 'DIS', 'TAX', 'PTRATIO', 'LSTAT']
+	label=col_names[-1]
+
+	print(feature_cols)
+	print(label)
+	X = dataset.loc[:,feature_cols]
+	print(X.shape)
+	y_test_pred=loaded_model.predict(X)
+	y_test=dataset[label]
+
+	from sklearn.preprocessing import MinMaxScaler
+	scaler = MinMaxScaler()
+	y_test_pred_rs = y_test_pred.reshape(-1,1)
+
+	scaler.fit(y_test_pred_rs)
+	y_test_pred_scaled = scaler.transform(y_test_pred_rs)
+	k = y_test_pred_scaled.shape
+	y_pred = y_test_pred_scaled.reshape(k[0])
+
+	y_test_np = np.asarray(y_test)
+	y_test_rs = y_test_np.reshape(-1,1)
+
+	scaler.fit(y_test_rs)
+	y_test_scaled = scaler.transform(y_test_rs)
+	k = y_test_scaled.shape
+	y_test_new = y_test_scaled.reshape(k[0])
+
+	# Metrics
+	r2 = metrics.r2_score(y_test, y_test_pred)
+	ar2 = 1 - (1-metrics.r2_score(y_test, y_test_pred))*(len(y_test)-1)/(len(y_test)-X.shape[1]-1)
+	mae = metrics.mean_absolute_error(y_test, y_test_pred)
+	mse = metrics.mean_squared_error(y_test, y_test_pred)
+	rmse = np.sqrt(metrics.mean_squared_error(y_test, y_test_pred))
+	rmsle = np.sqrt(metrics.mean_squared_log_error( y_test_new, y_pred ))
+	return render_template("evaluate_regression.html",msg="files have been Uploaded",mae=mae,mse=mse,rmse=rmse,rmsle=rmsle,r2=r2,ar2=ar2)
 
 
 
