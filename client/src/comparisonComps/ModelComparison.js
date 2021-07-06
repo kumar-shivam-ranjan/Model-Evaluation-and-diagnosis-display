08a885e12f9b6a327bed555ebb5d1ed7124ac36f
Kumar Shivam Ranjan <kshivamranjan.cse18@itbhu.ac.in>
2021-07-06 10:43:49 +0530

    Gunicorn integrated with docker

--- client/src/comparisonComps/ModelComparison.js
+++ client/src/comparisonComps/ModelComparison.js
@@ -3,14 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
-import Typography from '@material-ui/core/Typography';
+import {Typography, Paper, Box} from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ModelInfo from '../components/ModelInfo';
+import GeneralTable from '../comparisonComps/GeneralTable';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    paddingTop:'3%'
+    paddingTop: '3%',
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
@@ -21,13 +22,80 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
   },
-  tw:{
-      width:'100%'
-  }
+  tw: {
+    width: '100%',
+  },
 }));
 
+function createData(model, hyperparameters) {
+  return { model, ...hyperparameters };
+}
+
 export default function ModelComparison(props) {
-const evalList=props.evaluation;
+  const userDefinedHyperparametersList = new Set();
+  const evalList = props.evaluation;
+  const len = evalList.length;
+
+  // getting the union of all user defined hyperparameters
+  for(let i=0;i<len;i++){
+    if(evalList[i].data.model.metadata.hyperparameters == null ||
+      Object.keys(evalList[i].data.model.metadata.hyperparameters).length === 0)
+        continue;
+    for(let j=0;j<Object.keys(evalList[i].data.model.metadata.hyperparameters).length;j++){
+      userDefinedHyperparametersList.add(Object.keys(evalList[i].data.model.metadata.hyperparameters)[j]);
+    }
+  }
+  
+  //Setting up HeadCells of the table
+  const headCells = [];
+  headCells.push({id:'model', label:'Model'});
+  let userDefinedHyperparametersArray = [...userDefinedHyperparametersList];
+  for(let i=0; i< userDefinedHyperparametersArray.length; i++){
+    headCells.push({
+      id: i,
+      label: userDefinedHyperparametersArray[i]
+    });
+  }
+
+  // Setting up rows of the table
+  const rows=[];
+  for(let i=0;i<len;i++){
+    let tempRowValuesList = [];
+    if(evalList[i].data.model.metadata.hyperparameters == null ||
+    Object.keys(evalList[i].data.model.metadata.hyperparameters).length === 0){
+      for(let j=0;j<userDefinedHyperparametersArray.length;j++)
+        tempRowValuesList.push(null);
+    }
+    else{
+      for(let j=0;j<userDefinedHyperparametersArray.length;j++){
+        tempRowValuesList.push(null);
+      }
+      for(const [key, value] of Object.entries(evalList[i].data.model.metadata.hyperparameters)){
+        let columnId = headCells.filter((headCell) => headCell.label === key).map(headCell => headCell.id)[0];
+        // console.log(columnId);
+        if(value == null || value === true || value === false)
+          tempRowValuesList[columnId] = String(value);
+        else
+          tempRowValuesList[columnId] = value;
+      }
+      for(let j=0;j<evalList[i].data.model.metadata.keys.length;j++){
+        let column = headCells.filter((headCell) => headCell.label === evalList[i].data.model.metadata.keys[j])
+        let columnID = column?(column.map(headCell => headCell.id)[0]):-1;
+        if(columnID>0){
+          if(evalList[i].data.model.metadata.values[j] == null || 
+          evalList[i].data.model.metadata.values[j] === true || 
+          evalList[i].data.model.metadata.values[j] === false)
+            tempRowValuesList[columnID] = String(evalList[i].data.model.metadata.values[j]);
+          else
+            tempRowValuesList[columnID] = evalList[i].data.model.metadata.values[j];
+        }
+
+      } 
+    }
+    // console.log(tempRowValuesList);
+    rows.push(createData(evalList[i].data.model.name, tempRowValuesList));
+  }
+  console.log(headCells, rows);
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
@@ -38,58 +106,46 @@ const evalList=props.evaluation;
   return (
     <div className={classes.root}>
       <div className={classes.root}>
-        <h1 style={{color: 'indigo'}}>User specified HyperParamters: </h1>
-        {
-          evalList.map((evaluation, index) =>
-            <div key={index}>
-              <h2 style={{color: 'blue'}}>{evaluation.data.model.name}:</h2>
-              {(evaluation.data.model.metadata.hyperparameters == null || Object.keys(evaluation.data.model.metadata.hyperparameters).length === 0)? (
-                <i><h3>All hyperparametrs have default values</h3></i>
-              ):(
-                <ol>
-                  {Object.keys(evaluation.data.model.metadata.hyperparameters).map((key, index) => 
-                    <h3>
-                      <li key={index}>
-                        {key} = {evaluation.data.model.metadata.hyperparameters[key]}
-                      </li>
-                    </h3>
-                  )}
-                </ol>
-              )}
-              
-            </div>
-        )}
+        <Paper elevation={5}>
+          <Box m={2}>
+            <GeneralTable 
+              rows={rows}
+              headCells={headCells}
+              tabletitle="User Specified Hyperparameters"
+            />
+          </Box>
+        </Paper>
+        
       </div>
       <div className={classes.root}>
         <h1>All HyperParamters: </h1>
-        {
-          evalList.map((evaluation,index)=>(
-              <Accordion expanded={expanded === 'panel'+index} onChange={handleChange('panel'+index)}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"  
-                id="panel1bh-header"
-              >
-                <Typography className={classes.heading}><h4>{evaluation.data.model.name}</h4></Typography>
-  
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography  className={classes.tw}>
-        
-                    
-                    <ModelInfo
-                      standalone={0}
-                      keys={evaluation.data.model.metadata.keys}
-                      values={evaluation.data.model.metadata.values} 
-                    />
-              
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))
-        }
+        {evalList.map((evaluation, index) => (
+          <Accordion
+            expanded={expanded === 'panel' + index}
+            onChange={handleChange('panel' + index)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography className={classes.heading}>
+                <h4>{evaluation.data.model.name}</h4>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography className={classes.tw}>
+                <ModelInfo
+                  standalone={1}
+                  keys={evaluation.data.model.metadata.keys}
+                  values={evaluation.data.model.metadata.values}
+                  hyperparameters={evaluation.data.model.metadata.hyperparameters}
+                />
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </div>
     </div>
   );
-
 }
